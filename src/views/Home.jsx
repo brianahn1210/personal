a4aa77842ae8c85791742d726246c0f8d1ddcae6
Brian Ahn <jisolahn@gmail.com>
2024-04-14 02:12:08 -0500

    pdf and about me section

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -27,21 +27,17 @@ const Home = () => {
           <div className="sm:text-center lg:text-left">
             <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
               <motion.span
-                className={darkMode ? "block text-black" : " text-white"}
+                className={darkMode ? "font-normal md:text-4xl block text-black" : "font-normal md:text-4xl text-white"}
               >
-                Hi, I'm Brian Ahn
+                Hi there! <span className="block text-blue-500 z-0 lg:inline">I'm</span>
               </motion.span>
               <span className="block text-blue-500 z-0 lg:inline">
                 <Typical
                   steps={[
-                    "I'm studing Computer Engineering at UT Austin",
-                    1000,
-                    "I'm passionate about innovation and solving problems",
-                    1000,
-                    "I'm seeking a Full-Time role beginning in Fall 2025",
+                    "Brian Ahn",
                     1000,
                   ]}
-                  loop={Infinity}
+                  loop={1}
                 />
               </span>
             </h1>
@@ -67,9 +63,9 @@ const Home = () => {
             </div>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
-                <Link className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-                  Resume
-                </Link>
+                <a href="Brian_Ahn_Resume_1.pdf" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
+                  resume
+                </a>
               </div>
             </div>
           </div>
